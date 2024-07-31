b4b72c67915aabff236cd569f43e68282b0b33f9
Sushant <clashofgames229@gmail.com>
2024-07-31 22:43:13 +0545

    desgined footer partially

--- src/components/shared-component/Footer.tsx
+++ src/components/shared-component/Footer.tsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <footer className='bg-black text-slate-300 grid grid-cols-6'>
+        <section className='col-span-2'>
+            <img src='movie logo.png' alt='logo' className='h-16 w-16'/>
+        </section>
+        <section className='col-span-2 py-2     '>
+            <h1>Home</h1>
+            <h1>About</h1>
+            <h1>Contact</h1>
+            <p className='mb-0'>Copyright @ 2024</p>
+        </section>
+
+        <section className='col-span-2'>
+            skfs
+        </section>
+       
+       <section className='text-center items-center'>
+        <h3>Online Movie Ticket Booking System </h3>
+       </section>
+
+        
+   </footer>
   )
 }
 
--- src/components/shared-component/Navbar.tsx
+++ src/components/shared-component/Navbar.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className=' flex justify-between border-none border-slate-200 shadow-md py-2 px-2 items-center '>
+    <nav className=' flex justify-between border-none border-slate-200 shadow-md py-3 px-2 items-center '>
         <section className='mx-10 flex justify-normal gap-6'>
         
             <img src="movie logo.png" alt="Logo" className='h-8'/>
@@ -14,14 +14,14 @@ const Navbar = () => {
             return(
                 <div key={index} className=''>
                     <Link to={item.path} >
-                    <h2 className='flex justify-between text-2xl text-slate-700'>{item.title}</h2>
+                    <h2 className='flex justify-between text-2xl text-slate-700 font-semibold p-1 cursor-pointer hover:border-b-2 border-blue-800 transition-all ease-in-out delay-300'>{item.title}</h2>
                    </Link>
                 </div>
                
             )
         })}
         </section>
-         <section className='mx-10 text-2xl'>
+         <section className='mx-10 text-2xl font-semibold'>
                     
                     login
                 </section>
