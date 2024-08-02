6686b3e09c5b73b54e1d9be6f8acfbbafbec1975
Sushant <clashofgames229@gmail.com>
2024-08-02 20:21:27 +0545

    populating the homepage

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Carousel } from 'react-carousel-ts';
+
 
 const HomePage = () => {
+  const data= [
+    <div>1</div>,
+    <div>3</div>,
+    <div>3</div>
+  ];
   return (
    <main>
-    
+      <section>
+        {/* hero section */}
+        <Carousel items={data} className='carousel'/>
+      </section>
+
+      <section>
+        {/* Now Showing */}
+      </section>
+
+      <section>
+        {/* upcoming */}
+      </section>
    </main>
   )
 }
