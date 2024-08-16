cf5e7706140a81600ffa0a91b5269349269e9ff0
himalf <himal.fullel15@gmail.com>
2024-08-16 22:24:51 +0545

    navbar design

--- src/components/shared-component/Navbar.tsx
+++ src/components/shared-component/Navbar.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { NavData } from "../../data/navdata";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { IoIosArrowDropdownCircle } from "react-icons/io";
+import { IoIosArrowDropdownCircle, IoMdMenu, IoMdClose } from "react-icons/io";
 import { IUserDetail } from "@/types/UserDetail";
 import {
   DropdownMenu,
@@ -12,9 +12,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
+import logo from "../../../public/logo.jpg";
 const Navbar = () => {
   const [userData, setUserData] = useState<IUserDetail | null>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
 
   const fetchUser = async (userId: string | null) => {
@@ -42,54 +43,72 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex justify-between border-none border-slate-200 shadow-md py-3 px-2 items-center">
-      <section className="mx-10 flex justify-normal gap-6">
-        <img src="movie logo.png" alt="Logo" className="h-8" />
-        {NavData.map((item, index) => (
-          <div key={index}>
-            <Link to={item.path}>
-              <h2 className="flex justify-between text-2xl text-slate-700 font-semibold p-1 cursor-pointer hover:border-b-2 border-blue-800 transition-all ease-in-out delay-300">
-                {item.title}
-              </h2>
+    <nav className="flex flex-col md:flex-row justify-between items-center  shadow-md py-3 px-4 relative">
+      <div className="flex items-center justify-between w-full md:w-auto">
+        <img src={logo} alt="Logo" className=" rounded-full w-10 h-10" />
+        <button
+          className="text-3xl md:hidden"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          {isMenuOpen ? <IoMdClose /> : <IoMdMenu />}
+        </button>
+      </div>
+
+      <div
+        className={`md:flex flex-col md:flex-row md:items-center md:gap-6 w-full md:w-auto md:bg-transparent transition-transform  duration-300 ease-in-out ${
+          isMenuOpen ? "block" : "hidden"
+        }`}
+      >
+        <section className="flex flex-col md:flex-row md:gap-6 md:mx-10 md:justify-normal p-4 md:p-0">
+          {NavData.map((item, index) => (
+            <Link
+              key={index}
+              to={item.path}
+              className="text-xl font-semibold py-2 px-4 rounded hover:bg-gray-700 hover:text-white transition-colors duration-300"
+            >
+              {item.title}
             </Link>
-          </div>
-        ))}
-      </section>
-      <section className="mx-10 text-2xl font-semibold">
-        {userData ? (
-          <div className="flex items-center gap-4">
-            <div className="flex flex-col">
-              <p className="text-sm text-center">Welcome</p>{" "}
-              <h1 className="text-sm text-center"> {userData.fullname}</h1>
+          ))}
+        </section>
+
+        <section className="flex flex-col md:flex-row items-center md:mx-10 mt-4 md:mt-0 text-xl font-semibold">
+          {userData ? (
+            <div className="flex items-center gap-4 cursor-pointer">
+              <div className="flex flex-col text-sm text-center">
+                <p className="text-gray-400">Welcome</p>
+                <h1>{userData.fullname}</h1>
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <IoIosArrowDropdownCircle className="text-2xl" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel className="bg-gray-700 text-white">
+                    Hey, whatsup
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem className="cursor-pointer">
+                    Profile
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="cursor-pointer"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <IoIosArrowDropdownCircle />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>
-                  {" "}
-                  Hey, I'm {userData.fullname}
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem className="cursor-pointer">
-                  Profile
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  className="cursor-pointer"
-                  onClick={handleLogout}
-                >
-                  Logout
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        ) : (
-          <button>
-            <Link to="/login">Login</Link>
-          </button>
-        )}
-      </section>
+          ) : (
+            <Link
+              to="/login"
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors duration-300"
+            >
+              Login
+            </Link>
+          )}
+        </section>
+      </div>
     </nav>
   );
 };
